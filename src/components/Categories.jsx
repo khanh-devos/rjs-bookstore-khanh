76801f881b34398ad866d796596f5bc6ee2f4b41
khanh-devos <dreamproperty.khanh@gmail.com>
2023-07-24 23:33:40 +0700

    Add status 'Under construction' into category initialState

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,6 +3,5 @@ import { useSelector } from 'react-redux';
 export default function Categories() {
   const { categories } = useSelector((store) => store.category);
 
-  if (categories.length === 0) return (<div>Under Construction</div>);
-  return (<div>Categories</div>);
+  return (<div>{categories[0]}</div>);
 }
--- src/redux/categories/categorySlice.js
+++ src/redux/categories/categorySlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categories: [],
+  categories: ['Under construction'],
   amount: 0,
   isLoading: true,
 };
